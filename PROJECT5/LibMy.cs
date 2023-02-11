619209bae4790bb9f437d0d1134f1432d18f46f6
Zievidinov <108923651+RedkeyD@users.noreply.github.com>
2023-02-11 20:05:45 +0300

    Add project files.

---
+++ PROJECT5/LibMy.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT5
+{
+    static class LibMy
+    {
+        public static double Project1(double a, double b, double c)
+        {
+
+            double p = (a + b + c);
+
+            double K = (p * (p - a) * (p - b)) / a * b;
+
+            return K;
+        }
+        public static double Project2(double a, double b, int c)
+        {
+            double answer;
+            switch (c)
+            {
+                case 1:
+                    answer = a + b;
+                    return answer;
+                case 2:
+                    answer = a - b;
+                    return answer;
+                case 3:
+                    answer = a * b;
+                    return answer;
+                default:
+                    return 0;
+            }
+        }
+        public static double Project3(double xn, double xk, double x)
+        {
+            double S = 1, f;
+            int n, i;
+
+            for (n = 1; n <= xk*10; n++)
+            {
+                f = 1;
+                for (i = 1; i <= 2 * n; i++)
+                { f = f * i; }
+                S = S + Math.Pow(-1, n) * Math.Pow(x, 2 * n) / f;
+            }
+            return S;
+        }
+
+        public static double Project31(double xn, double xk, double x)
+        {
+            double Y = Math.Cos(x);
+            return Y;
+        }
+        public static int  Project4(int K, int L, int n, int[] mas)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (!(K <= i && i <= L))
+                {
+                    sum += mas[i];
+                }
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ PROJECT5/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using PROJECT5;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT5
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose what project you are want to see: ");
+            Console.WriteLine("1.First project");
+            Console.WriteLine("2.Second project");
+            Console.WriteLine("3.Third project");
+            Console.WriteLine("4.Fourth project");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please enter a, b and c: ");
+                    Console.WriteLine("Please enter a, b and c: ");
+                    Double a = Convert.ToDouble(Console.ReadLine());
+                    Double b = Convert.ToDouble(Console.ReadLine());
+                    Double c = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("K = " + LibMy.Project1(a, b, c));
+                    Console.ReadKey();
+
+                    break;
+                case 2:
+                    Console.WriteLine("Enter 1 digit: ");
+                    Double d = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter 2 digit: ");
+                    Double e = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Choose operation: ");
+                    Console.WriteLine("1.add ");
+                    Console.WriteLine("2.subtract ");
+                    Console.WriteLine("3.multiply ");
+                    int f = Convert.ToInt32(Console.ReadLine());
+
+                    if(f<1 && f > 3) { Console.WriteLine("Ошибка! Число должно быть в диапазоне от 1 до 3."); }
+
+                    Console.WriteLine((LibMy.Project2(d, e, f).ToString()));
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.Write("Enter a xn = ");
+                    double xn = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Enter a xk = ");
+                    double xk = Convert.ToDouble(Console.ReadLine());
+
+                    double h = (xk - xn) / 10;
+
+                    for (double x = xn; x <= xk; x += h)
+                    {
+                        double Y = LibMy.Project31(xn, xk, x);
+                        double S = LibMy.Project3(xn, xk, x);
+                        Console.WriteLine("When x = {0}, S(x) = {1}, Y(x) = {2}", x, S, Y);
+                    }
+                    Console.ReadKey();
+                        break;
+                case 4:
+                    Console.Write("Enter size of array n = ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int[] mas = new int[n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write("Enter element of array mas[{0}] = ", i);
+                        mas[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.Write("Enter a K = ");
+                    int K = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter a L = ");
+                    int L = Convert.ToInt32(Console.ReadLine());
+
+                    int sum = LibMy.Project4(K, L, n, mas);
+                    Console.WriteLine($"Sum of array is: {sum}");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Число должно быть в диапазоне от 1 до 4.");
+                    break;
+            }
+        }
+    }
+}
